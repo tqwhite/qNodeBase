ba4f57ffe337bfc1fac3e6bc7419b595936340e2
TQ White II <tq@justkidding.com>
2014-09-11 13:13:44 -0500

    changed dump to use utils.inspect, removed render package

--- qtools/qtools.js
+++ qtools/qtools.js
@@ -1,48 +1,55 @@
 'use strict';
-var events = require('events'), 
-	util=require('util'),
-//	require('environmentChanges'),
-	addBaseFunctions=require('qtoolsBase');
+var events = require('events'),
+	util = require('util'),
+//	environmentChanges = require('environmentChanges'),
+	addBaseFunctions = require('qtoolsBase');
 
 //START OF moduleFunction() ============================================================
 
-var moduleFunction=function(employer){
-		events.EventEmitter.call(this);
-		var self = this,
-			forceEvent = function(eventName, outData) {
-				this.emit(eventName, {
-					eventName: eventName,
-					data: outData
-				});
-			}
+var moduleFunction = function(employer) {
+	events.EventEmitter.call(this);
+	var self = this,
+		forceEvent = function(eventName, outData) {
+			this.emit(eventName, {
+				eventName: eventName,
+				data: outData
+			});
+		}
 
 		//INITIALIZE OBJECT ====================================
-		
-		addBaseFunctions(this);
-		
-		if (typeof(employer)=='object'){
-			this.employerFilePath=employer.filename?employer.filename:'';
-			
-			var split=this.employerFilePath.split('/');
 
-			if (split.length){
-				this.employerFilename=split[split.length-1];
-			}
-			else{
-				this.employerFilename='';
-			}	
+	addBaseFunctions(this);
+
+	if (typeof (employer) == 'object') {
+		this.employerFilePath = employer.filename ? employer.filename : '';
+
+		var split = this.employerFilePath.split('/');
+
+		if (split.length) {
+			this.employerFilename = split[split.length - 1];
+		} else {
+			this.employerFilename = '';
 		}
-		
+	}
 
 
-			
-		//BUILD RETURN OBJECT ====================================
+	this.listNames = function() {
+		for (var i in this) {
+			console.log(i);
+		}
+// 		for (var i = 0, len = environmentChanges.length; i < len; i++) {
+// 			var element = environmentChanges[i];
+// 			console.log(element);
+// 		}
+	}
 
-		this.forceEvent = forceEvent;
-		return this;
-	};
+	//BUILD RETURN OBJECT ====================================
+
+	this.forceEvent = forceEvent;
+	return this;
+};
 
 //END OF moduleFunction() ============================================================
 
 util.inherits(moduleFunction, events.EventEmitter);
-module.exports=moduleFunction;
+module.exports = moduleFunction;
